f0be877901f9309be7e3268e2734e253aba0a23c
Ravi <vyas.ravi.1991@gmail.com>
2020-05-21 16:17:08 -0700

    added routing:

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesListPage from './pages/ArticlesListPage';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div id="page-body">
-        <HomePage />
+    <BrowserRouter>
+      <div className="App">
+        <div id="page-body">
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/about">
+            <AboutPage />
+          </Route>
+          <Route path="/articles-list-page">
+            <ArticlesListPage />
+          </Route>
+          <Route path="/article/:name">
+            <ArticlePage />
+          </Route>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
